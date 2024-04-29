16734983150159fe1229e6feb3dee5cf0b77b5e6
Jnnsu <jspp120@gmail.com>
2024-04-30 02:41:54 +0900

    Chore: API url 수정

--- src/constants/apiUrl.ts
+++ src/constants/apiUrl.ts
@@ -2,6 +2,6 @@ export const URL = {
   BASE: 'https://api.pqsoft.net',
 
   SERVER: 'chat/v1/server',
-  USER: '/api/user/v1/user',
-  AUTH: '/api/user/v1/auth',
+  USER: '/user/v1/user',
+  AUTH: '/user/v1/auth',
 };
