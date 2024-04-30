aa8826a85a2089519e3cb999119753fb9016cd41
giho <giho7427@daum.net>
2024-04-30 19:45:08 +0900

    feat: 현재 mockData 채널 클릭시 해당 채널로 이동하는 라우팅 구현

--- src/pages/server/channel/Channel.tsx
+++ src/pages/server/channel/Channel.tsx
@@ -8,11 +8,11 @@ import { useParams } from 'react-router-dom';
 import VoiceChannel from './voiceChannel/VoiceChannel';
 
 export default function Channel() {
-  const { serverId, channeId } = useParams();
-  console.log(serverId, channeId);
-  // Channel의 종류에 따라 VoiceChannel, ChatChannel을 렌더링
-  const [channelType] = useState('chat');
-  return <Area>{channelType === 'chat' ? <ChatChannel /> : <VoiceChannel />}</Area>;
+  const { serverId, channelId } = useParams();
+  console.log('serverId:', serverId, 'channelId:', channelId);
+  /**@ToDo channel 데이터의 onVoice의 boolean에 따라 ChatChannel을 보여줄지 VoiceChannel을 보여줄지 생각 */
+  const [onVoice] = useState('false');
+  return <Area>{onVoice === 'false' ? <ChatChannel /> : <VoiceChannel />}</Area>;
 }
 
 const Area = styled.section`
