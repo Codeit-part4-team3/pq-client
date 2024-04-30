1666cb769f46aac1b367c0d1998723109e3a0308
giho <giho7427@daum.net>
2024-04-30 22:12:56 +0900

    feat: rtc 참여한 클라이언트에서 서버로 미디어 스트림 보내기 구현
    (cherry picked from commit a9b97c05ee4feffa1ba8009ac65487c3a9b5d18f)

--- src/pages/server/channel/Channel.tsx
+++ src/pages/server/channel/Channel.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 
 import styled from 'styled-components';
 
-import ChatChannel from './chatChannel/ChatChannel';
+// import ChatChannel from './chatChannel/ChatChannel';
 
 import { useParams } from 'react-router-dom';
 import VoiceChannel from './voiceChannel/VoiceChannel';
@@ -11,8 +11,13 @@ export default function Channel() {
   const { serverId, channelId } = useParams();
   console.log('serverId:', serverId, 'channelId:', channelId);
   /**@ToDo channel 데이터의 onVoice의 boolean에 따라 ChatChannel을 보여줄지 VoiceChannel을 보여줄지 생각 */
-  const [onVoice] = useState('false');
-  return <Area>{onVoice === 'false' ? <ChatChannel /> : <VoiceChannel />}</Area>;
+  // const [onVoice] = useState('false');
+  // return <Area>{onVoice === 'false' ? <ChatChannel /> : <VoiceChannel />}</Area>;
+  return (
+    <Area>
+      <VoiceChannel />
+    </Area>
+  );
 }
 
 const Area = styled.section`
--- src/pages/server/channel/voiceChannel/VoiceChannel.tsx
+++ src/pages/server/channel/voiceChannel/VoiceChannel.tsx
@@ -3,25 +3,101 @@ import styled from 'styled-components';
 import MyMediaControlPanel from './_components/MyMediaControlPanel';
 import Video from './_components/Video';
 import ChannelHeader from 'src/components/channel/ChannelHeader';
+import { useEffect, useRef, useState } from 'react';
+import { io, Socket } from 'socket.io-client';
+import { useParams } from 'react-router-dom';
 
-// @ToDo webRTC 버그 수정 전까지만 이 컴포넌트로 사용예정(일단 보여주기 식 입니다.)
+const SOCKET_SERVER_URL = 'http://localhost:3001';
+
+const pc_Config = {
+  iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
+};
+
+/**@ToDo 매일 시간내서 RTC 고치기 */
 export default function VoiceChannel() {
+  const { channelId: roomName } = useParams();
+  console.log('roomName:', roomName);
+  const socketRef = useRef<Socket | null>(null);
+  const senderPCRef = useRef<RTCPeerConnection | null>(null);
+  const receiverPCsRef = useRef<{ [socketId: string]: RTCPeerConnection }>({});
+  const localStreamRef = useRef<MediaStream | null>(null);
+  const localVideoRef = useRef<HTMLVideoElement | null>(null);
+  const [remoteStreams, setRemoteStreams] = useState<MediaStream[]>([]);
+
+  const getLocalMediaStream = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+      localStreamRef.current = stream;
+      if (localVideoRef.current) localVideoRef.current.srcObject = stream;
+
+      // peer connection 생성
+      senderPCRef.current = new RTCPeerConnection(pc_Config);
+
+      // localStream 추가
+      stream.getTracks().forEach((track) => senderPCRef.current?.addTrack(track, stream));
+
+      // ice candidate 전송
+      senderPCRef.current.onicecandidate = (e) => {
+        if (e.candidate) {
+          socketRef.current?.emit('newParticipant_ice_candidate', {
+            candidate: e.candidate,
+            newParticipantSocketId: socketRef.current?.id,
+            roomName,
+          });
+        }
+      };
+
+      // ice connection state 변경
+      senderPCRef.current.oniceconnectionstatechange = (e) => {
+        console.log('oniceconnectionstatechange :', e);
+      };
+
+      // Create and send offer
+      const offer = await senderPCRef.current.createOffer();
+      await senderPCRef.current.setLocalDescription(offer);
+      console.log('newParticipant_offer :', offer);
+      socketRef.current?.emit('newParticipant_offer', { offer, senderPCId: socketRef.current?.id, roomName });
+    } catch (error) {
+      console.error('getUserMedia Error:', error);
+    }
+  };
+
+  useEffect(() => {
+    socketRef.current = io(SOCKET_SERVER_URL);
+
+    socketRef.current.on('connect', async () => {
+      socketRef.current?.emit('join_voice_channel', { roomName });
+
+      socketRef.current?.on('newParticipant_answer', async ({ answer }) => {
+        console.log('newParticipant_answer :', answer);
+        if (senderPCRef.current) {
+          await senderPCRef.current.setRemoteDescription(new RTCSessionDescription(answer));
+        }
+      });
+
+      // 로컬 미디어 스트림 가져오기
+      getLocalMediaStream();
+    });
+
+    return () => {
+      socketRef.current?.disconnect();
+    };
+  }, []);
+
   return (
     <Wrapper>
       <ChannelHeader />
 
       <VideoContainer>
         <Video onVoice={true} />
-        <Video onVoice={false} />
-        <Video onVoice={false} />
-        <Video onVoice={false} />
+        <video ref={localVideoRef} autoPlay playsInline width={600} height={338}></video>
       </VideoContainer>
 
       <MyMediaControlPanel />
     </Wrapper>
   );
 }
-
+//
 const Wrapper = styled.div`
   width: 100%;
   height: 100%;
