b468e6a11ec51dc88bd58e4e820fe32475630f57
Jnnsu <jspp120@gmail.com>
2024-04-30 23:50:21 +0900

    Input 테스트 추가

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,6 +3,7 @@ module.exports = {
   testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
   moduleNameMapper: {
     '^src/(.*)$': '<rootDir>/src/$1',
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/src/pages/signup/_test/image.mocks.ts',
   },
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
