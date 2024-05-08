f6ddeaab385c514affac1473e5715a432c6576e4
giho <giho7427@daum.net>
2024-05-08 23:50:05 +0900

    feat: 무한스크롤 안되는 버그 수정

--- src/pages/server/channel/chatChannel/ChatChannel.tsx
+++ src/pages/server/channel/chatChannel/ChatChannel.tsx
@@ -283,7 +283,7 @@ export default function ChatChannel() {
 
 const Wrapper = styled.div`
   width: 100%;
-  height: 100%;
+  height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
